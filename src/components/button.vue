babd2379cf10d10442f0637dd6518696490d5223
9huit <nantenkf@gmail.com>
2025-04-02 22:16:45 +0300

    up

---
+++ src/components/button.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+    <button>Bonjour</button>
+</template>
+
+<style lang="scss" scoped>
+    .button{
+    @include bouton($primary, $secondary, $radius-pm, $stara);
+    transition: all 0.5s ease-in-out;
+    }
+    .button:hover{
+    font-weight: 700;
+    }
+</style>
